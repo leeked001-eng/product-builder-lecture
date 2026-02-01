7c0db91f805006092833ab6010275e8db3a7b8ce
leeked001 <leeked001@gmail.com>
2026-02-01 10:12:04 +0000

    feat: Add dark mode/light mode toggle

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const themeSwitch = document.getElementById('checkbox');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.setAttribute('data-theme', currentTheme);
+
+        if (currentTheme === 'dark') {
+            themeSwitch.checked = true;
+        }
+    }
+
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.body.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }
+    }
+
+    themeSwitch.addEventListener('change', switchTheme, false);
+
     const generateBtn = document.getElementById('generate-btn');
     const lottoNumbersContainer = document.querySelector('.lotto-numbers');
 
